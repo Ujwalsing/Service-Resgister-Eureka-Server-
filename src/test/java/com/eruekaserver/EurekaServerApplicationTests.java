8ab88795ec03a3e10f82c75267f4e6a43d32811d
Ujwalsing <ujwalsinghs987@gmail.com>
2024-06-27 15:46:45 +0530

    Eureka Server

---
+++ src/test/java/com/eruekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eruekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
